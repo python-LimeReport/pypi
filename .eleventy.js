6e036857417666ae544180dfed9e55654f5c6283
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-09-10 10:27:24 +0000

    add html-minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+      if (outputPath.endsWith(".html")) {
+        let minified = htmlmin.minify(content, {
+          useShortDocType: true,
+          removeComments: true,
+          collapseWhitespace: true,
+        })
+
+        return minified;
+      }
+    })
+
     return {
         dir: {
           input: 'src',
